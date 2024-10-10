159cacf2729bdcfa31e42c7879b517871364a259
juliaborssato <borssatojulia@gmail.com>
2024-10-10 09:37:12 -0300

    mapa ok

--- src/navigation/drawer.navigation.tsx
+++ src/navigation/drawer.navigation.tsx
@@ -1,10 +1,10 @@
 import { createDrawerNavigator, DrawerNavigationProp } from '@react-navigation/drawer';
 import { colors } from '../styles/colors';
-import { AntDesign, FontAwesome, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { AntDesign, FontAwesome, FontAwesome5, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 import { TabNavigation } from './tab.navigation';
 import { Camera } from '../screens/Camera';
 import { Imagens } from '../screens/Imagens';
-import { QrCode } from '../screens';
+import { LocationMap, QrCode } from '../screens';
 import React from 'react';
 
 type DrawerParamList = {
@@ -12,6 +12,7 @@ type DrawerParamList = {
     Camera: undefined
     Imagem: undefined
     QrCode: undefined
+    LocationMap: undefined
 }
 type DrawerScreenNavigationProp = DrawerNavigationProp<DrawerParamList, 'Tab'>
 export type DrawerTypes = {
@@ -59,6 +60,13 @@ export function DrawerNavigation() {
                     ),
                 }}
                 />
+            <Drawer.Screen name='LocationMap' component={LocationMap}
+                options={{
+                    drawerIcon: () => (
+                        <FontAwesome5 name="map-marked-alt" size={24} color={colors.white} />
+                    ),
+                }}
+            />
         </Drawer.Navigator>
     )
 }
